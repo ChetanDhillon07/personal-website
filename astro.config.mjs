89f345dec7226779f0477ba73c6b34ad2ffa38e8
ChetanDhillon07 <99910244+ChetanDhillon07@users.noreply.github.com>
2026-07-26 01:27:05 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
-
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ubiquitous-cobbler-3828e7.netlify.app/',
   vite: {
+    css: {
+      transformer: 'lightningcss', // 👈 Required for optimal Tailwind v4 processing
+    },
     plugins: [tailwindcss()]
   },
-
   integrations: [mdx(), sitemap()]
 });
-
