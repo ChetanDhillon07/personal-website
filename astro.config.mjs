5974d59115f71bfb580dda27e0eab62a8f8d7c03
ChetanDhillon07 <99910244+ChetanDhillon07@users.noreply.github.com>
2026-07-26 01:22:52 +0530

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://alex.dev',
+  site: 'https://ubiquitous-cobbler-3828e7.netlify.app/',
   vite: {
     plugins: [tailwindcss()]
   },
