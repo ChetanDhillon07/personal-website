5b1798ec52549e5064083dbb21108ab97b7a1919
ChetanDhillon07 <99910244+ChetanDhillon07@users.noreply.github.com>
2026-07-26 01:19:55 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,11 +8,11 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ChetanDhillon07.github.io',
-  base: '/personal-website',
+  site: 'https://alex.dev',
   vite: {
     plugins: [tailwindcss()]
   },
 
   integrations: [mdx(), sitemap()]
 });
+
